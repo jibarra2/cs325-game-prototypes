b541c59d2b98f0a2623f035e593874ca9a77d072
Jonathan Ibarra <Jonathan@Jonathans-MacBook-Pro-2.local>
2020-04-08 18:00:22 -0400

    7th push

--- Digital Game 5/js/main.js
+++ Digital Game 5/js/main.js
@@ -1,3 +1,15 @@
+"use strict";
+
+window.onload = function() {
+    // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
+    // You will need to change the fourth parameter to "new Phaser.Game()" from
+    // 'phaser-example' to 'game', which is the id of the HTML element where we
+    // want the game to go.
+    // The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
+    // You will need to change the paths you pass to "game.load.image()" or any other
+    // loading functions to reflect where you are putting the assets.
+    // All loading functions will typically all be found inside "preload()".
+    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
     var Breakout = new Phaser.Class({
 
         Extends: Phaser.Scene,
@@ -15,7 +27,7 @@
     
         preload: function ()
         {
-            this.load.atlas('assets', 'assets/breakout.png', 'assets/breakout.json');
+            this.load.atlas('assets', 'assets/games/breakout/breakout.png', 'assets/games/breakout/breakout.json');
         },
     
         create: function ()
@@ -124,16 +136,5 @@
         }
     
     });
-    
-    var config = {
-        type: Phaser.WEBGL,
-        width: 800,
-        height: 600,
-        parent: "game",
-        scene: [ Breakout ],
-        physics: {
-            default: 'arcade'
-        }
-    };
-    
-    var game = new Phaser.Game(config);
+
+};
