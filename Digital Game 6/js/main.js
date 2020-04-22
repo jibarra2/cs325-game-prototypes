3d7887b11bbd66d7e23bdb890f451a11f2cd3a0c
Jonathan Ibarra <Jonathan@Jonathans-MacBook-Pro-2.local>
2020-04-22 19:28:03 -0400

    still not loading

--- Digital Game 6/js/main.js
+++ Digital Game 6/js/main.js
@@ -41,7 +41,7 @@ window.onload = function() {
     }
 
     function preload() {
-        this.load.atlas('assets', 'assets/breakout.png', 'assets/breakout.json');
+        this.load.atlas('elements', 'assets/breakout.png', 'assets/breakout.json');
     }
     function create() {
           //  Enable world bounds, but disable the floor
@@ -49,15 +49,15 @@ window.onload = function() {
 
           //  Create the bricks in a 10x6 grid
           this.bricks = this.physics.add.staticGroup({
-              key: 'assets', frame: [ 'blue1', 'red1', 'green1', 'yellow1', 'silver1', 'purple1' ],
+              key: 'elements', frame: [ 'blue1', 'red1', 'green1', 'yellow1', 'silver1', 'purple1' ],
               frameQuantity: 10,
               gridAlign: { width: 10, height: 6, cellWidth: 64, cellHeight: 32, x: 112, y: 100 }
           });
   
-          this.ball = this.physics.add.image(400, 500, 'assets', 'ball1').setCollideWorldBounds(true).setBounce(1);
+          this.ball = this.physics.add.image(400, 500, 'elements', 'ball1').setCollideWorldBounds(true).setBounce(1);
           this.ball.setData('onPaddle', true);
   
-          this.paddle = this.physics.add.image(400, 550, 'assets', 'paddle1').setImmovable();
+          this.paddle = this.physics.add.image(400, 550, 'elements', 'paddle1').setImmovable();
   
           //  Our colliders
           this.physics.add.collider(this.ball, this.bricks, this.hitBrick, null, this);
