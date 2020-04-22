a4aa47bd9e7c9ce5d6ae729dc4ab547e4e79fdd9
Jonathan Ibarra <Jonathan@Jonathans-MacBook-Pro-2.local>
2020-04-22 19:57:10 -0400

    deleting everything

--- Digital Game 6/js/main.js
+++ Digital Game 6/js/main.js
@@ -15,13 +15,8 @@ window.onload = function() {
         height: 600,
         parent: 'game',
         scene: {
-            Breakout: Breakout,
             preload: preload,
             create: create,
-            hitBrick: hitBrick,
-            resetBall: resetBall,
-            resetLevel: resetLevel,
-            hitPaddle: hitPaddle,
             update: update
         },
         physics: {
@@ -30,118 +25,20 @@ window.onload = function() {
     };
     
     var game = new Phaser.Game(config);
-    
-    function Breakout ()
-    {
-        Phaser.Scene.call(this, { key: 'breakout' });
-
-        this.bricks;
-        this.paddle;
-        this.ball;
-    }
 
     function preload() {
-        this.load.atlas('elements', 'assets/breakout.png', 'assets/breakout.json');
         game.load.image('paddle', 'assets/paddle1.png');
     }
+
     function create() {
           //  Enable world bounds, but disable the floor
           this.physics.world.setBoundsCollision(true, true, true, false);
 
           game.add.sprite(400, 550, 'paddle');
-          
-          //  Create the bricks in a 10x6 grid
-          this.bricks = this.physics.add.staticGroup({
-              key: 'elements', frame: [ 'blue1', 'red1', 'green1', 'yellow1', 'silver1', 'purple1' ],
-              frameQuantity: 10,
-              gridAlign: { width: 10, height: 6, cellWidth: 64, cellHeight: 32, x: 112, y: 100 }
-          });
-  
-          this.ball = this.physics.add.image(400, 500, 'elements', 'ball1').setCollideWorldBounds(true).setBounce(1);
-          this.ball.setData('onPaddle', true);
-  
-          this.paddle = this.physics.add.image(400, 550, 'elements', 'paddle1').setImmovable();
-  
-          //  Our colliders
-          this.physics.add.collider(this.ball, this.bricks, this.hitBrick, null, this);
-          this.physics.add.collider(this.ball, this.paddle, this.hitPaddle, null, this);
-  
-          //  Input events
-          this.input.on('pointermove', function (pointer) {
-  
-              //  Keep the paddle within the game
-              this.paddle.x = Phaser.Math.Clamp(pointer.x, 52, 748);
-  
-              if (this.ball.getData('onPaddle'))
-              {
-                  this.ball.x = this.paddle.x;
-              }
-  
-          }, this);
-  
-          this.input.on('pointerup', function (pointer) {
-  
-              if (this.ball.getData('onPaddle'))
-              {
-                  this.ball.setVelocity(-75, -300);
-                  this.ball.setData('onPaddle', false);
-              }
-  
-          }, this);
-      }
 
-    function hitBrick(ball, brick) {
-        brick.disableBody(true, true);
-
-        if (this.bricks.countActive() === 0)
-        {
-            this.resetLevel();
-        }
     }
-
-    function resetBall() {
-        this.ball.setVelocity(0);
-        this.ball.setPosition(this.paddle.x, 500);
-        this.ball.setData('onPaddle', true);
-    }
-
-    function resetLevel() {
-        this.resetBall();
-
-        this.bricks.children.each(function (brick) {
-
-            brick.enableBody(false, 0, 0, true, true);
-
-        });
-    }
-
-    function hitPaddle(ball, paddle) {
-        var diff = 0;
-
-        if (ball.x < paddle.x)
-        {
-            //  Ball is on the left-hand side of the paddle
-            diff = paddle.x - ball.x;
-            ball.setVelocityX(-10 * diff);
-        }
-        else if (ball.x > paddle.x)
-        {
-            //  Ball is on the right-hand side of the paddle
-            diff = ball.x -paddle.x;
-            ball.setVelocityX(10 * diff);
-        }
-        else
-        {
-            //  Ball is perfectly in the middle
-            //  Add a little random X to stop it bouncing straight up!
-            ball.setVelocityX(2 + Math.random() * 8);
-        }
-    }
-
+    
     function update() {
-        if (this.ball.y > 600)
-        {
-            this.resetBall();
-        }
+
     }
 };
