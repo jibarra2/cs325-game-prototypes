fc3a8d62c715b5a0efa0efd908776ca64ae81dd4
Jonathan Ibarra <Jonathan@Jonathans-MacBook-Pro-2.local>
2020-04-22 23:04:14 -0400

    changing stock logo

--- Digital Game 6/js/main.js
+++ Digital Game 6/js/main.js
@@ -17,7 +17,7 @@ window.onload = function() {
     
     function preload() {
         // Load an image and call it 'logo'.
-        game.load.image( 'logo', 'assets/phaser.png' );
+        game.load.image( 'Tornado', 'assets/Tornado.png' );
         // load a tilemap and call it 'map'.
         // from .json file
         game.load.tilemap('map', 'assets/tilemap_example.json', null, Phaser.Tilemap.TILED_JSON);
@@ -52,7 +52,7 @@ window.onload = function() {
 
         
         // Create a sprite at the center of the screen using the 'logo' image.
-        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
+        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'Tornado' );
         // Anchor the sprite at its center, as opposed to its top-left corner.
         // so it will be truly centered.
         bouncy.anchor.setTo( 0.5, 0.5 );
