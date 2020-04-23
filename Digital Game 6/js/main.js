288f4363313ab283a78f055d7b9ad9e6a55674cd
Jonathan Ibarra <Jonathan@Jonathans-MacBook-Pro-2.local>
2020-04-22 22:45:02 -0400

    starting over

--- Digital Game 6/js/main.js
+++ Digital Game 6/js/main.js
@@ -1,6 +1,7 @@
-"use strict";
-
 window.onload = function() {
+    // You might want to start with a template that uses GameStates:
+    //     https://github.com/photonstorm/phaser/tree/v2.6.2/resources/Project%20Templates/Basic
+    
     // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
     // You will need to change the fourth parameter to "new Phaser.Game()" from
     // 'phaser-example' to 'game', which is the id of the HTML element where we
@@ -9,16 +10,46 @@ window.onload = function() {
     // You will need to change the paths you pass to "game.load.image()" or any other
     // loading functions to reflect where you are putting the assets.
     // All loading functions will typically all be found inside "preload()".
+    
+    "use strict";
+    
     var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
     
     function preload() {
         // Load an image and call it 'logo'.
-        game.load.image( 'logo', 'assets1/phaser.png' );
+        game.load.image( 'logo', 'assets/phaser.png' );
+        // load a tilemap and call it 'map'.
+        // from .json file
+        game.load.tilemap('map', 'assets/tilemap_example.json', null, Phaser.Tilemap.TILED_JSON);
+        // alternatively, from .csv file
+        //game.load.tilemap('map', 'assets/tilemap_example.csv', null, Phaser.Tilemap.CSV);
+        
+        //load tiles for map
+        game.load.image('tiles', 'assets/tiles.png');
     }
     
+    var map;
+    var layer1;
     var bouncy;
     
     function create() {
+        // Create the map. 
+        map = game.add.tilemap('map');
+        // for csv files specify the tile size.
+        //map = game.add.tilemap('map', 32, 32);
+        
+        //add tiles
+        map.addTilesetImage('tiles');
+        
+        // Create a layer from the map
+        //using the layer name given in the .json file
+        layer1 = map.createLayer('Tile Layer 1');
+        //for csv files
+        //layer1 = map.createLayer(0);
+        
+        //  Resize the world
+        layer1.resizeWorld();
+        
         // Create a sprite at the center of the screen using the 'logo' image.
         bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
         // Anchor the sprite at its center, as opposed to its top-left corner.
@@ -33,8 +64,12 @@ window.onload = function() {
         // Add some text using a CSS style.
         // Center it in X, and position its top 15 pixels from the top of the world.
         var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        var text = game.add.text( game.world.centerX, 15, "Build something amazing.", style );
+        var text = game.add.text( 400, 15, "Build something amazing.", style );
+        text.fixedToCamera = true;
         text.anchor.setTo( 0.5, 0.0 );
+        
+        game.camera.follow(bouncy);
+        
     }
     
     function update() {
@@ -43,6 +78,6 @@ window.onload = function() {
         // in X or Y.
         // This function returns the rotation angle that makes it visually match its
         // new trajectory.
-        bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, game.input.activePointer, 500, 500, 500 );
+        bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, this.game.input.activePointer, 500, 500, 500 );
     }
 };
