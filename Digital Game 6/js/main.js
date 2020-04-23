267ccffcd05654be1c4cda39eb6c216c48c59492
Jonathan Ibarra <Jonathan@Jonathans-MacBook-Pro-2.local>
2020-04-23 05:48:48 -0400

    cant get collect cars function

--- Digital Game 6/js/main.js
+++ Digital Game 6/js/main.js
@@ -1,30 +1,15 @@
 window.onload = function() {
-    // You might want to start with a template that uses GameStates:
-    //     https://github.com/photonstorm/phaser/tree/v2.6.2/resources/Project%20Templates/Basic
-    
-    // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
-    // You will need to change the fourth parameter to "new Phaser.Game()" from
-    // 'phaser-example' to 'game', which is the id of the HTML element where we
-    // want the game to go.
-    // The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
-    // You will need to change the paths you pass to "game.load.image()" or any other
-    // loading functions to reflect where you are putting the assets.
-    // All loading functions will typically all be found inside "preload()".
     
     "use strict";
     
-    const game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
-        preload: preload,
-        create: create,
-        update: update
-    })
-    
+    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
+    let bouncy
     let house1
     let house2
     let mailbox
     let cars
     let beds
-    let bouncy
+   
 
     function preload() {
         // Load an image and call it 'logo'.
@@ -118,7 +103,7 @@ window.onload = function() {
         bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, this.game.input.activePointer, 500, 500, 500 );
     }
 
-    function collectCar( bouncy, cars){
+    function collectCar( bouncy, cars) {
         // Removes the car from the screen
         cars.kill()
     }
